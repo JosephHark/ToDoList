1d634a839c7506a5fe0277813410ef5972949c60
JosephHark <har17033@byui.edu>
2022-05-01 21:25:27 -0400

    5/1

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const userController = require('../controllers/item');
+const userController = require('../controllers/user');
 
 router.get('/', userController.getAll);
 
