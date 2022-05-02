c2a2ea38c2aef4305bc5d1b893ef3d4e2f450b20
JosephHark <har17033@byui.edu>
2022-05-01 21:29:07 -0400

    5/1

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,7 @@ const router = express.Router();
 
 router.use('/', require('./swagger'));
 router.use('/user', require('./user'));
+router.use('/item', require('./item'));
+
 
 module.exports = router;
